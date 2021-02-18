df90e27a88b3749b3ed5f948a9199679c47fc36a
Joseph Lev-Tov <jtlevtov@gmail.com>
2021-02-18 16:36:01 -0700

    Did some gaming. Event system set up. No deck system.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -28,8 +28,42 @@ class Event(db.Model):
         return '<Event %r>' % self.name
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    events = Event.query.all()
+    return render_template('index.html', events=events)
+    # return render_template('index.html')
+
+@app.route('/addEvent/', methods=['POST', 'GET'])
+def addEvent():
+    if request.method == 'POST':
+        name = request.form['eventName']
+        details = request.form['eventDetails']
+        print(name)
+        print(details)
+        newEvent = Event(name=name, details=details)
+        try:
+            db.session.add(newEvent)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an error adding your event."
+    else:
+        return render_template('addEvent.html')
+
+@app.route('/eventDetails/<int:id>')
+def eventDetails(id):
+    event = Event.query.get_or_404(id)
+    return render_template('eventDetails.html', name=event.name, details=event.details)
+
+@app.route('/delete/<int:id>')
+def deleteEvent(id):
+    event = Event.query.get_or_404(id)
+    try:
+        db.session.delete(event)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was an error deleting your event"
 
 
 if __name__ == '__main__':
